5fd73984b6e328dcfd4b6802114f938907a00a26
janna-credot <janna@credot.in>
2025-01-07 09:44:06 +0530

    change in order history page

--- src/pages/Orders/OrderHistory.tsx
+++ src/pages/Orders/OrderHistory.tsx
@@ -152,9 +152,13 @@ const Orders = () => {
                 <Link to={`/orders/detail/${order._id}`} key={order._id}>
                  <div className="order-card" >
                   <div className="order-card-header">
-                    <span className={`order-status ${order.shippingStatus.toLowerCase().replace(/\s+/g, '')}`}>{order.shippingStatus
+                  {order.shippingStatus === "SHIPPED" ? (
+                       <span className={`order-status ${order.shippingStatus.toLowerCase().replace(/\s+/g, '')}`}>Out for Delivery</span>
+                    ):(
+                      <span className={`order-status ${order.shippingStatus.toLowerCase().replace(/\s+/g, '')}`}>{order.shippingStatus
                         .toLowerCase()
                         .replace(/(?:^|\s)\w/g, (match:string) => match.toUpperCase())}</span>
+                    )}
                     <h5 className="order-id">
                         <span className="label">Order ID:</span> 
                         <span className="value">{order.orderId}</span>
